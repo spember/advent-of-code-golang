a9c8b31613eeab775f251e43f70b889b56541bb7
Steve Pember <spember@gmail.com>
2024-12-10 08:36:35 -0500

    -let's not run the slow test

--- internal/aoc2024/aoc202407/puzzle_test.go
+++ internal/aoc2024/aoc202407/puzzle_test.go
@@ -23,5 +23,5 @@ func TestPart2(t *testing.T) {
 	assert.Equal(t, int64(11387), Part2(sampleInput))
 	// failed: 24217859095482
 	// this is not the right answer ... so no star for me. Some slight bug that I ran out of time looking for
-	assert.Equal(t, int64(24217859095482), Part2(input))
+	//assert.Equal(t, int64(24217859095482), Part2(input))
 }
